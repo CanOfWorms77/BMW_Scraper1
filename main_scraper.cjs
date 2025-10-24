f3d58b3d6b7d86afa73bae14818358c8ca6bbd6b
David Holt <d_gholt@hotmail.com>
2025-10-24 19:38:08 +0100

    Update main_scraper.cjs

--- main_scraper.cjs
+++ main_scraper.cjs
@@ -231,10 +231,9 @@ async function navigateAndFilter(page, currentModel, auditPath) {
     await page.click('#series');
     await page.waitForTimeout(1200);*/
 
-    await page.waitForSelector('#react-select-2-input', { timeout: 60000 });
-    await page.click('#react-select-2-input');
+    await page.waitForSelector('#series > div.uvl-c-react-select__control.css-1fnsmel-control', { timeout: 60000 });
+    await page.click('#series > div.uvl-c-react-select__control.css-1fnsmel-control');
     await page.waitForTimeout(1200);
-
 
     let matched = false;
 
