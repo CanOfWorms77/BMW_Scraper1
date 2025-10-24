4ee8957f3709b96354ad4152ebe461a46db0c17b
David Holt <d_gholt@hotmail.com>
2025-10-24 18:00:14 +0100

    Update modelSelectorTable.js

--- utils/modelSelectorTable.js
+++ utils/modelSelectorTable.js
@@ -1,7 +1,6 @@
 // utils/modelSelectorTable.js
 const modelSelectorTable = {
   'X5': {
-    seriesIndex: 9,
     bodyStyleIndex: 4,
     skipBodyStyle: false,
     variant: '50e',
@@ -9,7 +8,6 @@ const modelSelectorTable = {
     seriesIndex: 'X'
   },
   '5 Series': {
-    seriesIndex: 4,
     skipBodyStyle: true,
     variant: '550e',
     modeltext: '550e xDrive',
